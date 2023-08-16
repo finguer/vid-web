04fc90c45105702e8e5aa9681e31a21b43763079
favio-effex <67810006+favio-effex@users.noreply.github.com>
2023-08-16 12:16:18 -0400

    adding changes

--- js/theme.js
+++ js/theme.js
@@ -5,7 +5,7 @@ function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _ty
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 /**
- * Cartzilla | Bootstrap E-Commerce Template
+ * vid | Bootstrap E-Commerce Template
  * Copyright 2023 Createx Studio
  * Theme core scripts
  * 
